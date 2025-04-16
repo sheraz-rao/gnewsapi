48309618d7ccbeba66ee75c1ef4a9a545aa5d017
sheraz-rao <muhammadsherazrao@gmail.com>
2025-04-16 22:51:32 +0500

    added corsheaders

--- GNewsAPI/settings.py
+++ GNewsAPI/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'news',
     'rest_framework',
 ]
@@ -46,10 +47,15 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React dev server
 ]
 
 ROOT_URLCONF = 'GNewsAPI.urls'
